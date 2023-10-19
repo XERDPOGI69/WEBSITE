42dbb22772c672deb42829332644b723e4690772
XERDPOGI69 <joshportacion9@gmail.com>
2023-10-19 22:10:02 +0800

    First commit

---
+++ WEB.js
@@ -0,0 +1,25 @@
+window.addEventListener("load", function () {
+  const isDarkMode = localStorage.getItem("darkMode");
+  if (isDarkMode === "enabled") {
+    document.body.classList.add("dark-theme");
+    document.getElementById("icon").src = "Dark.png";
+  }
+});
+
+
+var icon = document.getElementById("icon");
+icon.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    icon.src = "IMAGES/Dark.png";
+    localStorage.setItem("darkMode", "enabled");
+  } else {
+    icon.src = "IMAGES/Light.png";
+    localStorage.setItem("darkMode", "disabled");
+  }
+};
+
+const logo = document.getElementById("logo");
+logo.addEventListener("click", function () {
+  window.location.href = "WEB.html";
+});
